d8cfbb285e18399d7983b23ac49fa1b546bef408
Technologie31 <78386526+Technologie31@users.noreply.github.com>
2022-04-01 13:14:10 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
+})
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
 	
 })
